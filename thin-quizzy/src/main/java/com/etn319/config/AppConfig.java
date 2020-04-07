d57cf2c3e3a8676366ec677a16880c3aa3539533
artyom-319 <etn319@gmail.com>
2020-04-08 00:03:47 +0300

    Константы вынесены в файл .properties

--- thin-quizzy/src/main/java/com/etn319/config/AppConfig.java
+++ thin-quizzy/src/main/java/com/etn319/config/AppConfig.java
@@ -6,20 +6,29 @@ import com.etn319.service.QuestionConverter;
 import com.etn319.service.QuestionCsvConverter;
 import com.etn319.service.QuestionFileSource;
 import com.etn319.service.QuestionSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class AppConfig {
+
+    @Value("${csv.delimiter}")
+    private String csvDelimiter;
+
+    @Value("${question.source.filename}")
+    private String questionsFileName;
 
     @Bean
     public QuestionConverter questionConverter() {
-        return new QuestionCsvConverter(";");
+        return new QuestionCsvConverter(csvDelimiter);
     }
 
     @Bean
     public QuestionSource questionSource(QuestionConverter converter) {
-        return new QuestionFileSource(converter, "questions.csv");
+        return new QuestionFileSource(converter, questionsFileName);
     }
 
     @Bean
