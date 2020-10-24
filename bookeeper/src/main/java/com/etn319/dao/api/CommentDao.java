dba25a770d78d898739e2209e49b54b2891f1e68
artyom-f <etn019@mail.ru>
2020-10-24 22:01:20 +0300

    добавил методов в CommentDao

--- bookeeper/src/main/java/com/etn319/dao/api/CommentDao.java
+++ bookeeper/src/main/java/com/etn319/dao/api/CommentDao.java
@@ -4,8 +4,48 @@ import com.etn319.model.Book;
 import com.etn319.model.Comment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentDao {
+
+    /**
+     * Количество комментариев в базе
+     * @return количество комментариев в базе
+     */
+    long count();
+
+    /**
+     * Получить комментарий по id
+     * @param id идентификатор искомого комментария
+     * @return объект комментария, упакованный в <code>Optional</code>
+     * <code>Optional.empty()</code>, если не найден
+     */
+    Optional<Comment> getById(long id);
+
+    /**
+     * Сохраняет комментарий в базе. Если задан ненулевой ID, произойдёт вставка в таблицу, в противном случае - обновление
+     * @param comment объект комментария
+     * @return сохранённый в базе объект комментария
+     * @throws com.etn319.dao.DaoLayerException, если нарушено какое-либо ограничение таблицы
+     * @throws com.etn319.dao.EntityNotFoundException, если по id не найдено записей
+     */
+    Comment save(Comment comment);
+
+    /**
+     * Удаляет комментарий из базы. Если задан нулевой id, ничего не произойдёт
+     * @param comment комментарий, которую нужно удалить
+     * @throws com.etn319.dao.DaoLayerException, если удаление не удалось выполнить
+     */
+    void delete(Comment comment);
+
+    /**
+     * Удаляет комментарий из базы по его id
+     * @param id id комментария, который нужно удалить
+     * @throws com.etn319.dao.DaoLayerException, если удаление не удалось выполнить
+     * @throws com.etn319.dao.EntityNotFoundException, если по id не найдено записей
+     */
+    void deleteById(long id);
+
     /**
      * Получить комментарии к книге
      * @param book книга
--- bookeeper/src/main/java/com/etn319/dao/jpa/CommentDaoJpaImpl.java
+++ bookeeper/src/main/java/com/etn319/dao/jpa/CommentDaoJpaImpl.java
@@ -1,5 +1,7 @@
 package com.etn319.dao.jpa;
 
+import com.etn319.dao.DaoLayerException;
+import com.etn319.dao.EntityNotFoundException;
 import com.etn319.dao.api.CommentDao;
 import com.etn319.model.Book;
 import com.etn319.model.Comment;
@@ -7,13 +9,59 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class CommentDaoJpaImpl implements CommentDao {
     @PersistenceContext
     private EntityManager em;
+
+    @Override
+    public long count() {
+        TypedQuery<Long> query = em.createQuery("select count(c) from Comment c", long.class);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Optional<Comment> getById(long id) {
+        return Optional.ofNullable(em.find(Comment.class, id));
+    }
+
+    @Override
+    public Comment save(Comment comment) {
+        if (comment.getId() == 0L) {
+            em.persist(comment);
+            em.flush();
+        } else
+            em.merge(comment);
+        return comment;
+    }
+
+    @Override
+    public void delete(Comment comment) {
+        try {
+            em.remove(comment);
+        } catch (RuntimeException e) {
+            throw new DaoLayerException(e);
+        }
+    }
+
+    @Override
+    public void deleteById(long id) {
+        int deleted;
+        try {
+            Query query = em.createQuery("delete from Comment c where c.id = :id");
+            query.setParameter("id", id);
+            deleted = query.executeUpdate();
+        } catch (RuntimeException e) {
+            throw new DaoLayerException(e);
+        }
+        if (deleted == 0)
+            throw new EntityNotFoundException();
+    }
 
     @Override
     public List<Comment> getCommentsByBook(Book book) {
