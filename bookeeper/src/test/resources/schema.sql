4667141c77a099ee3e06fe4da9765d7e2e5510a7
artyom-f <etn019@mail.ru>
2020-10-24 20:01:55 +0300

    добавил секцию jpa в test application.yml, добавил создание таблицы комментов в schema.sql

--- bookeeper/src/test/resources/schema.sql
+++ bookeeper/src/test/resources/schema.sql
@@ -16,7 +16,15 @@ create table books (
             primary key,
     title     varchar(1024),
     author_id bigint
-        constraint books_author_fkey references authors on delete cascade ,
+        constraint books_author_fkey references authors ,
     genre_id  bigint
-        constraint books_genre_fkey references genres on delete cascade
+        constraint books_genre_fkey references genres
+);
+
+create table comments (
+    id          bigserial not null constraint books_pkey primary key,
+    text        varchar(1024),
+    commenter   varchar(256),
+    book_id     bigint
+        constraint comments_book_fkey references books
 );
