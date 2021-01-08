a0f11332a47b28862ad71958ba6cc077f04708f7
artyom-f <etn019@mail.ru>
2021-01-08 19:26:49 +0300

    rest controllers

---
+++ bookeeper/src/main/java/com/etn319/web/controllers/rest/AuthorRestController.java
@@ -0,0 +1,93 @@
+package com.etn319.web.controllers.rest;
+
+import com.etn319.model.Author;
+import com.etn319.service.common.EmptyMandatoryFieldException;
+import com.etn319.service.common.api.AuthorService;
+import com.etn319.service.common.api.BookService;
+import com.etn319.web.NotFoundException;
+import com.etn319.web.dto.AuthorDto;
+import com.etn319.web.dto.BookDto;
+import com.etn319.web.dto.mappers.AuthorMapper;
+import com.etn319.web.dto.mappers.BookMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.etn319.web.dto.mappers.AuthorMapper.toDomainObject;
+import static com.etn319.web.dto.mappers.AuthorMapper.toDto;
+
+@RestController
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class AuthorRestController {
+    private final AuthorService service;
+    private final BookService bookService;
+
+    @GetMapping("/api/authors")
+    public List<AuthorDto> list() {
+        return service.getAll()
+                .stream()
+                .map(AuthorMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/authors/{id}")
+    public AuthorDto details(@PathVariable("id") String id) {
+        return service.getById(id)
+                .map(AuthorMapper::toDto)
+                .orElseThrow(() -> new NotFoundException("Author not found: id=" + id));
+    }
+
+    @GetMapping("/api/authors/{id}/books")
+    public List<BookDto> listBooksOfAuthor(@PathVariable("id") String id) {
+        return bookService.getByAuthorId(id)
+                .stream()
+                .map(BookMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/api/authors")
+    public ResponseEntity<AuthorDto> newAuthor(@RequestBody AuthorDto authorDto) {
+        Author savedAuthor = service.save(toDomainObject(authorDto));
+        return ResponseEntity
+                .created(URI.create("/authors"))
+                .body(toDto(savedAuthor));
+    }
+
+    @PutMapping("/api/authors")
+    public ResponseEntity<AuthorDto> updateAuthor(@RequestBody AuthorDto authorDto) {
+        // todo: exists в сервисах
+        if (service.getById(authorDto.getId()).isEmpty()) {
+            throw new NotFoundException("Author id=" + authorDto.getId() + " does not exist");
+        }
+        Author savedAuthor = service.save(toDomainObject(authorDto));
+        return ResponseEntity
+                .ok()
+                .body(toDto(savedAuthor));
+    }
+
+    @DeleteMapping("/api/authors/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") String id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(EmptyMandatoryFieldException.class)
+    public ResponseEntity<String> emptyMandatoryFieldHandler(Throwable t) {
+        return ResponseEntity
+                .badRequest()
+                .body(t.getMessage());
+    }
+}
---
+++ bookeeper/src/main/java/com/etn319/web/controllers/rest/BookRestController.java
@@ -0,0 +1,77 @@
+package com.etn319.web.controllers.rest;
+
+import com.etn319.model.Book;
+import com.etn319.service.common.api.BookService;
+import com.etn319.web.NotFoundException;
+import com.etn319.web.dto.BookDto;
+import com.etn319.web.dto.CommentDto;
+import com.etn319.web.dto.mappers.BookMapper;
+import com.etn319.web.dto.mappers.CommentMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.etn319.web.dto.mappers.BookMapper.toDomainObject;
+import static com.etn319.web.dto.mappers.BookMapper.toDto;
+
+@RestController
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class BookRestController {
+    private final BookService service;
+
+    @GetMapping("/api/books")
+    public List<BookDto> list() {
+        return service.getAll()
+                .stream()
+                .map(BookMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/books/{id}")
+    public BookDto list(@PathVariable("id") String id) {
+        Book bookDomain = service.getById(id)
+                .orElseThrow(() -> new NotFoundException("Book not found: id=" + id));
+        List<CommentDto> comments = bookDomain.getComments()
+                .stream()
+                .map(CommentMapper::toDto)
+                .collect(Collectors.toList());
+        BookDto book = toDto(bookDomain);
+        book.setComments(comments);
+        return book;
+    }
+
+    @PostMapping("/api/books")
+    public BookDto newBook(@RequestBody BookDto bookDto) {
+        Book savedBook = service.save(toDomainObject(bookDto));
+        return toDto(savedBook);
+    }
+
+    @PutMapping("/api/books")
+    public ResponseEntity<BookDto> updateBook(@RequestBody BookDto bookDto) {
+        // todo: exists в сервисах
+        if (service.getById(bookDto.getId()).isEmpty()) {
+            throw new NotFoundException("Book id=" + bookDto.getId() + " does not exist");
+        }
+        Book savedBook = service.save(toDomainObject(bookDto));
+        return ResponseEntity
+                .ok()
+                .body(toDto(savedBook));
+    }
+
+    @DeleteMapping("/api/books/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable("id") String bookId) {
+        service.deleteById(bookId);
+        return ResponseEntity.noContent().build();
+    }
+}
