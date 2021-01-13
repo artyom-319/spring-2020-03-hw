70a921cf545261c95a472e0579289e9cda413a76
artyom-319 <etn319@gmail.com>
2021-01-13 18:28:21 +0300

    + controller tests

---
+++ bookeeper/src/test/java/com/etn319/web/controllers/rest/AuthorRestControllerTest.java
@@ -0,0 +1,106 @@
+package com.etn319.web.controllers.rest;
+
+import com.etn319.model.Author;
+import com.etn319.service.common.api.AuthorService;
+import com.etn319.web.dto.AuthorDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthorRestController.class)
+class AuthorRestControllerTest {
+    private ObjectWriter jackson = new ObjectMapper().writer().withDefaultPrettyPrinter();
+    private List<Author> all = List.of(
+            new Author("id1", "name1", "country1"),
+            new Author("id2", "name2", "country2"),
+            new Author("id3", "name3", "country3")
+    );
+    @MockBean
+    private AuthorService service;
+    @Autowired
+    private MockMvc mvc;
+
+    @BeforeEach
+    void setUp() {
+        given(service.getAll()).willReturn(all);
+        given(service.first()).willReturn(Optional.of(all.get(0)));
+        given(service.getById(ArgumentMatchers.anyString())).willReturn(Optional.empty());
+        given(service.getById("id1")).willReturn(Optional.of(all.get(0)));
+        given(service.getById("id2")).willReturn(Optional.of(all.get(1)));
+        given(service.getById("id3")).willReturn(Optional.of(all.get(2)));
+        given(service.save(any(Author.class)))
+                .willAnswer(inv -> inv.getArgument(0));
+    }
+
+    @Test
+    void getAllAuthors_shouldReturnServiceResult() throws Exception {
+        mvc.perform(get("/api/authors/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].id",
+                        is(List.of("id1", "id2", "id3"))))
+                .andExpect(jsonPath("$[*].name",
+                        is(List.of("name1", "name2", "name3"))));
+    }
+
+    @Test
+    void getAuthor_shouldReturnSingleResult() throws Exception {
+        mvc.perform(get("/api/authors/id1/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].id", is(emptyList())))
+                .andExpect(jsonPath("$.id", is("id1")))
+                .andExpect(jsonPath("$.name", is("name1")));
+    }
+
+    @Test
+    void getNotExistingAuthor_shouldReturn404() throws Exception {
+        mvc.perform(get("/api/authors/id0/"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void postAuthor_shouldPassDomainAuthorToService() throws Exception {
+        AuthorDto dto = AuthorDto.builder()
+                .name("new-author")
+                .country("new-country")
+                .build();
+        ArgumentCaptor<Author> captor = ArgumentCaptor.forClass(Author.class);
+        mvc.perform(
+                post("/api/authors/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(jackson.writeValueAsString(dto)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is("new-author")));
+        verify(service).save(captor.capture());
+        assertThat(captor.getValue())
+                .extracting(Author::getName, Author::getCountry)
+                .containsExactly("new-author", "new-country");
+    }
+}
---
+++ bookeeper/src/test/java/com/etn319/web/controllers/rest/BookRestControllerTest.java
@@ -0,0 +1,98 @@
+package com.etn319.web.controllers.rest;
+
+import com.etn319.model.Book;
+import com.etn319.service.common.api.BookService;
+import com.etn319.web.NotFoundException;
+import com.etn319.web.dto.BookDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookRestController.class)
+class BookRestControllerTest {
+    private static final String EXISTING_ID = "EXISTING_ID";
+    private static final String NOT_EXISTING_ID = "NOT_EXISTING_ID";
+    private static final String SAVED_ID = "SAVED_ID";
+    private static final String SAVED_TITLE = "SAVED_TITLE";
+    private static final Book SAVED = new Book(SAVED_ID, SAVED_TITLE, null, null);
+    private ObjectWriter jackson = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+    @MockBean
+    private BookService service;
+    @Autowired
+    private MockMvc mvc;
+
+    @BeforeEach
+    void setUp() {
+        given(service.save(any())).willReturn(SAVED);
+        // todo: отказаться в пользу exists
+        given(service.getById(anyString())).willReturn(Optional.of(new Book()));
+        doNothing().when(service).deleteById(EXISTING_ID);
+        doThrow(NotFoundException.class).when(service).deleteById(NOT_EXISTING_ID);
+    }
+
+    @Test
+    void putBook_ShouldPassBookToService() throws Exception {
+        BookDto dto = BookDto.builder()
+                .id("id")
+                .title("new-title")
+                .build();
+        ArgumentCaptor<Book> captor = ArgumentCaptor.forClass(Book.class);
+        mvc.perform(
+                put("/api/books/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(jackson.writeValueAsString(dto)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(SAVED_ID)))
+                .andExpect(jsonPath("$.title", is(SAVED_TITLE)));
+        verify(service).save(captor.capture());
+        assertThat(captor.getValue())
+                .extracting(Book::getId, Book::getTitle)
+                .containsExactly("id", "new-title");
+    }
+
+    @Test
+    void deleteBook_ShouldCallServiceDeleteMethod() throws Exception {
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        mvc.perform(delete("/api/books/{id}/", EXISTING_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        verify(service).deleteById(captor.capture());
+        assertThat(captor.getValue()).isEqualTo(EXISTING_ID);
+    }
+
+    @Test
+    void deleteNotExistingBook_ShouldReturn404() throws Exception {
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        mvc.perform(delete("/api/books/{id}/", NOT_EXISTING_ID))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+        verify(service).deleteById(captor.capture());
+        assertThat(captor.getValue()).isEqualTo(NOT_EXISTING_ID);
+    }
+}
