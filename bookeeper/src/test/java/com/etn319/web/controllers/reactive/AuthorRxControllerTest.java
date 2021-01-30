0b565da6ad49ca11cf3e2707bf697c63d841c245
artyom-f <etn019@mail.ru>
2021-01-30 17:08:27 +0300

    AuthorRxControllerTest

--- bookeeper/src/test/java/com/etn319/web/controllers/reactive/AuthorRxControllerTest.java
+++ bookeeper/src/test/java/com/etn319/web/controllers/reactive/AuthorRxControllerTest.java
@@ -3,6 +3,7 @@ package com.etn319.web.controllers.reactive;
 import com.etn319.dao.mongo.reactive.AuthorReactiveMongoRepository;
 import com.etn319.dao.mongo.reactive.BookReactiveMongoRepository;
 import com.etn319.web.dto.AuthorDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
@@ -10,9 +11,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
 @WebFluxTest(AuthorRxController.class)
 public class AuthorRxControllerTest {
-    private AuthorDto dto = new AuthorDto();
+    private static final String EXISTING_ID = "EXISTING_ID";
 
     @Autowired
     private WebTestClient client;
@@ -22,24 +27,71 @@ public class AuthorRxControllerTest {
     @MockBean
     private BookReactiveMongoRepository bookRepository;
 
+    @BeforeEach
+    void setUp() {
+        when(repository.existsById(anyString())).thenReturn(Mono.just(false));
+        when(repository.existsById(eq(EXISTING_ID))).thenReturn(Mono.just(true));
+        when(repository.deleteById(eq(EXISTING_ID))).thenReturn(Mono.empty());
+    }
+
     @Test
     void postAuthorWithEmptyName__ShouldReturn400() {
+        var dto = new AuthorDto();
         client.post()
                 .uri("/rx/api/authors")
                 .bodyValue(dto)
                 .exchange()
                 .expectStatus()
-                .isCreated();
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("Author name cannot be empty");
     }
 
+    @Test
+    void putAuthorWithEmptyName__ShouldReturn400() {
+        var dto = new AuthorDto();
+        dto.setId(EXISTING_ID);
+        client.put()
+                .uri("/rx/api/authors")
+                .bodyValue(dto)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("Author name cannot be empty");
+    }
 
     @Test
-    void customTest() {
-        String block = Mono.just("MyString")
-                .filter(a -> a.length() > 10)
-                .switchIfEmpty(Mono.error(new RuntimeException("AAAAA")))
-                .log()
-                .block();
-        System.out.println(block);
+    void putAuthorThatDoesNotExist__ShouldReturn400() {
+        var dto = new AuthorDto("invalid_id", "name", "country");
+        client.put()
+                .uri("/rx/api/authors")
+                .bodyValue(dto)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("Author id=" + dto.getId() + " does not exist");
+    }
+
+    @Test
+    void deleteAuthorThatDoesNotExist__ShouldReturn400() {
+        var invalidId = "invalid_id";
+        client.delete()
+                .uri("/rx/api/authors/" + invalidId)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("Author id=" + invalidId + " does not exist");
+    }
+
+    @Test
+    void deleteExistingAuthor__ShouldReturn204() {
+        client.delete()
+                .uri("/rx/api/authors/" + EXISTING_ID)
+                .exchange()
+                .expectStatus()
+                .isNoContent();
     }
 }
