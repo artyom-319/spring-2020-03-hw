7f0a598c848ebc1d16e10c86d6f7a191ec724f54
artyom-319 <etn319@gmail.com>
2021-01-30 15:48:32 +0300

    trying to make author rx controller test pass

---
+++ bookeeper/src/test/java/com/etn319/web/controllers/reactive/AuthorRxControllerTest.java
@@ -0,0 +1,45 @@
+package com.etn319.web.controllers.reactive;
+
+import com.etn319.dao.mongo.reactive.AuthorReactiveMongoRepository;
+import com.etn319.dao.mongo.reactive.BookReactiveMongoRepository;
+import com.etn319.web.dto.AuthorDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(AuthorRxController.class)
+public class AuthorRxControllerTest {
+    private AuthorDto dto = new AuthorDto();
+
+    @Autowired
+    private WebTestClient client;
+
+    @MockBean
+    private AuthorReactiveMongoRepository repository;
+    @MockBean
+    private BookReactiveMongoRepository bookRepository;
+
+    @Test
+    void postAuthorWithEmptyName__ShouldReturn400() {
+        client.post()
+                .uri("/rx/api/authors")
+                .bodyValue(dto)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+
+    @Test
+    void customTest() {
+        String block = Mono.just("MyString")
+                .filter(a -> a.length() > 10)
+                .switchIfEmpty(Mono.error(new RuntimeException("AAAAA")))
+                .log()
+                .block();
+        System.out.println(block);
+    }
+}
