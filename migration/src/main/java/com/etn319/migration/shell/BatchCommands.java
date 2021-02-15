33ec4cc91ac82a3ef0b1d48193c9283c63b779f0
artyom-f <etn019@mail.ru>
2021-02-16 01:18:18 +0300

    shell commands

---
+++ migration/src/main/java/com/etn319/migration/shell/BatchCommands.java
@@ -0,0 +1,43 @@
+package com.etn319.migration.shell;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+
+@RequiredArgsConstructor
+@ShellComponent
+public class BatchCommands {
+
+    private final Job importArtistsJob;
+
+    private final JobLauncher jobLauncher;
+    private final JobOperator jobOperator;
+    private final JobExplorer jobExplorer;
+
+    //http://localhost:8080/h2-console/
+
+    @ShellMethod(value = "startMigrationJobWithJobLauncher", key = "sm-jl")
+    public void startMigrationJobWithJobLauncher() throws Exception {
+        JobExecution execution = jobLauncher.run(importArtistsJob, new JobParametersBuilder().toJobParameters());
+        System.out.println(execution);
+    }
+
+    @ShellMethod(value = "startMigrationJobWithJobOperator", key = "sm-jo")
+    public void startMigrationJobWithJobOperator() throws Exception {
+        Long executionId = jobOperator.start("importArtistsJob", "");
+        System.out.println(jobOperator.getSummary(executionId));
+    }
+
+    @ShellMethod(value = "showInfo", key = "i")
+    public void showInfo() {
+        System.out.println(jobExplorer.getJobNames());
+        System.out.println(jobExplorer.getLastJobInstance("importArtistsJob"));
+    }
+}
